c6b6338d9d71336e031209d31fe494e2d856c0e3
Nayel-Blidi <nayel.blidi@gmail.com>
2025-05-29 15:09:48 +0200

    Fix: test bucket init

--- pylcloud/storage/test/mini-io.py
+++ pylcloud/storage/test/mini-io.py
@@ -3,12 +3,11 @@ import os, sys
 MAIN_DIR = os.path.dirname(os.path.dirname((os.path.dirname(__file__))))
 if __name__ == "__main__":
     sys.path.append(MAIN_DIR)
-print(MAIN_DIR)
 
 from storage import StorageMiniIO
 
-
+# test upload of this __file__.py
 dir = os.path.dirname(__file__)
 api_miniio = StorageMiniIO(bucket_name="test")
-
+api_miniio.create_bucket()
 api_miniio.upload_files(paths=__file__)
